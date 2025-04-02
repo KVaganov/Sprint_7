eeefb8b520546fe30524b2025191411f6b6d3343
Kirill Vaganov <fapfapdng@mail.ru>
2025-04-02 22:20:28 +0300

    commit

--- src/main/java/OrderGeneration.java
+++ src/main/java/OrderGeneration.java
@@ -1,2 +1,15 @@
-package PACKAGE_NAME;public class OrderGeneration {
+public class OrderGeneration {
+    static String[] colors;
+
+    public static Orders getDefault() {
+        return new Orders("Уважаемый",
+                "Рьвьюер",
+                "Yandex",
+                1,
+                "89998887766",
+                5,
+                "2025-04-02",
+                "Зачтите работу пожалуйста",
+                colors);
+    }
 }
