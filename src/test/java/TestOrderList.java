b0513c3ae18024040cb890b75ddc9a334cb8d853
Kirill Vaganov <fapfapdng@mail.ru>
2025-04-04 16:57:36 +0300

    commit1

--- src/test/java/TestOrderList.java
+++ src/test/java/TestOrderList.java
@@ -3,7 +3,9 @@ import io.restassured.response.ValidatableResponse;
 import org.junit.Test;
 
 import static org.apache.http.HttpStatus.SC_OK;
-import static org.hamcrest.CoreMatchers.notNullValue;
+import static org.hamcrest.CoreMatchers.*;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.collection.IsEmptyCollection.empty;
 import static org.junit.Assert.assertEquals;
 
 public class TestOrderList {
@@ -16,5 +18,8 @@ public class TestOrderList {
         int statusCode = response.extract().statusCode();
         assertEquals("Статус код не 200", SC_OK, statusCode);
         response.assertThat().body("orders.id", notNullValue());
+
+        // Проверка, что список заказов не пуст
+        assertThat(response.extract().body().jsonPath().getList("orders"), is(not(empty())));
     }
 }
